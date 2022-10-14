166064b68e286646d9f06e9d5118c57582e663ac
--replace-all <cogh0020@algonquinlive.com>
2022-10-14 14:49:36 -0400

    Removed commented code from header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,11 @@
-//import React, {useState, useEffect} from 'react'
 import NavBar from '../NavBar/NavBar'
 
 function Header(){
-
-
-  // useEffect(()=>{
-  //   if (){
-  //     //pass the value back up to app
-  //   }
-  // }, [searchValue])
-
-  // function handleSubmit(ev){
-  //   ev.preventDefault()
-  //   console.log("Form submitted")
-  //   setSearchValue(ev.target[0].value)
-  //   this.props.onSearch(ev.target[0].value)
-  // }
 
   return(
     <header className="App-header">
         <h2 className="app-title">Weather App</h2>
         <NavBar/>
-        
     </header>
   )
 }
